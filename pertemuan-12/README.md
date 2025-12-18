a25a3a08b82c2f9674456111a8bde04d4c04a930
afdal <2511500077@atmaluhur.ac.id>
2025-12-18 17:54:27 +0700

    mencopy file pertemuan 11 ke pertemuan 12

---
+++ pertemuan-12/README.md
@@ -0,0 +1,10 @@
+# pertemuan-10
+silahkan sesuaikan dengan milik kalian masing-masing:<br><br>
+Hari ini, selasa, 16-12-2025<br>
+NIM: 2511500077<br>
+Nama: Afdal<br>
+Kelompok: gb1j<br>
+<ol>
+  <li>mencopy file pertemuan 11 ke pertemuan 12</li>
+  
+</ol>
--- pertemuan-12/read.php
+++ pertemuan-12/read.php
@@ -1,5 +1,6 @@
 <?php
 require 'koneksi.php';
+require 'fungsi.php';
 
 $sql = "SELECT * FROM tbl_tamu ORDER BY cid DESC";
 $q = mysqli_query($conn, $sql) ;
@@ -22,27 +23,18 @@ $no = 1;
 
 <?php while ($row = mysqli_fetch_assoc($q)): ?>
 <tr>
-    <!-- No -->
+
     <td><?= $no++; ?></td>
 
-    <!-- Aksi -->
     <td>
         <a href="edit.php?id=<?= $row['cid']; ?>">edit</a>
     </td>
 
-    <!-- ID -->
     <td><?= $row['cid']; ?></td>
-
-    <!-- Nama -->
     <td><?= htmlspecialchars($row['cnama']); ?></td>
-
-    <!-- Email -->
     <td><?= htmlspecialchars($row['cemail']); ?></td>
-
-    <!-- Pesan -->
     <td><?= htmlspecialchars($row['cpesan']); ?></td>
 
-    <!-- Created At -->
     <td>
         <?= date('d M Y H:i:s', strtotime($row['created_at'])); ?>
     </td>
